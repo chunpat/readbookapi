8d27faab1e76904b99d9600d274b13aed686c924
哈哈哈 <2867557054@qq.com>
2022-03-17 05:49:19 +0000

    update app/common.php.

--- app/common.php
+++ app/common.php
@@ -56,6 +56,29 @@ function createJwt($userid){
         $jwt = \Firebase\JWT\JWT::encode($token, $key,'HS256');
         return $jwt;
     }
+    function verifyJwt($jwt = ''){
+        $key = md5('adsga#@!');
+        try {
+            $jwtAuth = json_encode(\Firebase\JWT\JWT::decode($jwt, new \Firebase\JWT\Key($key, 'HS256')));
+            $authInfo = json_decode($jwtAuth, true);
+    
+            $msg = [];
+            if (!empty($authInfo['userid'])) {
+                return $authInfo;
+            } 
+            
+            
+            return 'Token验证不通过,用户不存在';
+            
+        }  catch (\Firebase\JWT\ExpiredException $e) {
+            return 'Token过期';
+            
+            
+        } catch (\Exception $e) {
+            return  'Token无效';
+            
+        }
+    }
 // app登录
 function createJwtMember($userid){
         $key = md5('234aef1$ae#@!'); //jwt的签发密钥，验证token的时候需要用到
